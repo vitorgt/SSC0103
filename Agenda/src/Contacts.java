1ae240200147845ece560fb643b485637593f048
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-04 10:08:48 -0300

    Create Contacts.java

---
+++ Agenda/src/Contacts.java
@@ -0,0 +1,12 @@
+
+public class Contacts{
+
+	private String name, address, email;
+
+	public Contacts(String name, String address, String email){
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
+
+}
