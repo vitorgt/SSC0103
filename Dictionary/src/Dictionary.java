be9863db26b3c8e2f028717c5d145e7673b5b7b3
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-24 10:59:01 -0300

    Update Dictionary.java

--- Dictionary/src/Dictionary.java
+++ Dictionary/src/Dictionary.java
@@ -4,14 +4,22 @@ public class Dictionary<K,V>{
 	private K k[];
 	private V v[];
 	private int n = 0;
-	
+
 	@SuppressWarnings("unchecked")
-	public Dictionary(){
-		k = (K[]) new Object[100];
-		v = (V[]) new Object[100];
-	}
-	
+		public Dictionary(){
+			k = (K[]) new Object[10];
+			v = (V[]) new Object[10];
+		}
+
+	@SuppressWarnings("unchecked")
+		public Dictionary(int n){
+			k = (K[]) new Object[n];
+			v = (V[]) new Object[n];
+		}
+
 	public void add(K newk, V newv){
+		if(n == v.length)
+			throw new IndexOutOfBoundsException();
 		k[n] = newk;
 		v[n] = newv;
 		n++;
@@ -23,19 +31,29 @@ public class Dictionary<K,V>{
 				return v[i];
 		return null;
 	}
-	
+
 	public boolean containsKey(K s){
-		for(int i = 0; i < n; i++)
-			if(k[i].equals(s))
-				return true;
-		return false;
+		return search(s) != null;
+		/*
+		   for(int i = 0; i < n; i++)
+			   if(k[i].equals(s))
+				   return true;
+		   return false;
+		 */
 	}
-	
+
 	public boolean containsValue(V s){
-		for(int i = 0; i < n; i++)
-			if(v[i].equals(s))
-				return true;
+		for(V l : v){
+			if(l == null) return false;
+			if(l.equals(s)) return true;
+		}
 		return false;
+		/*
+		   for(int i = 0; i < n; i++)
+			   if(v[i].equals(s))
+				   return true;
+		   return false;
+		 */
 	}
 
 }
