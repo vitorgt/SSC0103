b8035ce7d44c8537fdf08867ec0031c3e88f505b
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-24 10:59:33 -0300

    Update DictionaryTest.java

--- Dictionary/src/DictionaryTest.java
+++ Dictionary/src/DictionaryTest.java
@@ -5,49 +5,60 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DictionaryTest {
-	
+
 	private Dictionary<Integer,String> test;
 
 	@Before
-	public void setUp() throws Exception {
-		test = new Dictionary<Integer,String>();
-		test.add(4952, "Vitor");
-		test.add(4667, "Fabio");
-	}
+		public void setUp() throws Exception {
+			test = new Dictionary<Integer,String>();
+			test.add(4952, "Vitor");
+			test.add(4667, "Fabio");
+		}
 
 	@After
-	public void tearDown() throws Exception {
-		test = null;
-	}
-	
-	@Test
-	public void testSearch() {
-		assertEquals("Fabio", test.search(4667));
-	}
-	
-	@Test
-	public void testSearchNull() {
-		assertNull(test.search(1563));
-	}
+		public void tearDown() throws Exception {
+			test = null;
+		}
 
 	@Test
-	public void testContainsKey() {
-		assertTrue(test.containsKey(4952));
-	}
+		public void testDictionaryInt() {
+			Dictionary<Integer,String> test = new Dictionary<Integer,String>(50);
+		}
+
+	@Test (expected = IndexOutOfBoundsException.class)
+		public void testAddFull() {
+			for(int i = 1; i < 12; i++)
+				test.add(i, ""+(char)(i+60));
+		}
 
 	@Test
-	public void testContainsKeyNotFound() {
-		assertFalse(test.containsKey(1563));
-	}
+		public void testSearch() {
+			assertEquals("Fabio", test.search(4667));
+		}
 
 	@Test
-	public void testContainsValue() {
-		assertTrue(test.containsValue("Fabio"));
-	}
+		public void testSearchNull() {
+			assertNull(test.search(1563));
+		}
 
 	@Test
-	public void testContainsValueNotFound() {
-		assertFalse(test.containsValue("Joao"));
-	}
+		public void testContainsKey() {
+			assertTrue(test.containsKey(4952));
+		}
+
+	@Test
+		public void testContainsKeyNotFound() {
+			assertFalse(test.containsKey(1563));
+		}
+
+	@Test
+		public void testContainsValue() {
+			assertTrue(test.containsValue("Fabio"));
+		}
+
+	@Test
+		public void testContainsValueNotFound() {
+			assertFalse(test.containsValue("Joao"));
+		}
 
 }
