5a13d13db0195d559557b56f6205dd995b9cf6df
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-26 08:57:49 -0300

    Update ContaBancaria.java

--- ContasBancarias/src/ContaBancaria.java
+++ ContasBancarias/src/ContaBancaria.java
@@ -1,5 +1,5 @@
 
-public abstract class ContaBancaria {
+public abstract class ContaBancaria implements Comparable<ContaBancaria> {
 
 	public static int n = 0;
 	private int conta;
@@ -13,26 +13,33 @@ public abstract class ContaBancaria {
 		this.saldo = 0;
 		this.correntista = correntista;
 	}
+	
 	public static int getQuantidadeContas(){
 		return n;
 	}
+	
 	public int getNumConta() {
 		return conta;
 	}
+	
 	public double getSaldo() {
 		return saldo;
 	}
+	
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+	
 	public String getNomeCliente() {
 		return correntista;
 	}
+	
 	public void deposita(double deposito){
 		if(deposito > 0){
 			this.saldo += deposito;
 		}
 	}
+	
 	protected void saca(double saque) throws IllegalArgumentException {
 		if(saldo > saque){
 			this.saldo -= saque;
@@ -41,6 +48,31 @@ public abstract class ContaBancaria {
 			throw new IllegalArgumentException("Insufficient funds");
 		}
 	}
+	
 	public abstract void atualiza(double tx);
+	
+	@Override
+	public int compareTo(ContaBancaria b){
+		if(b == null){
+			System.out.println("if null");
+			return 0;
+		}
+		if(		this instanceof ContaEspecial && b instanceof ContaEspecial ||
+				this instanceof ContaPoupancaOuro && b instanceof ContaPoupancaOuro ||
+				this instanceof ContaPoupancaSimples && b instanceof ContaPoupancaSimples){
+			System.out.println("primeiro if");
+			if(this.getSaldo() == b.getSaldo()) return 0;
+			if(this.getSaldo() < b.getSaldo()) return -1;
+			return 1;
+			
+		}
+		else{
+			System.out.println("segundo if");
+			if(		this instanceof ContaEspecial && b instanceof ContaPoupancaOuro ||
+					this instanceof ContaPoupancaOuro && b instanceof ContaPoupancaSimples)
+				return -1;
+			else return 1;
+		}
+	}
 
 }
