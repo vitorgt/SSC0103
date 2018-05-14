d98a8f83dd3c73823d2cfbe9c2094318b39d2868
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-05-14 20:17:00 -0300

    Create GUIMain.java

---
+++ Bozo/src/GUIMain.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+public class GUIMain {
+	
+	private static GUIBozo bozo = null;
+	private static GUIDados dados = null;
+	public static RolaDados rola = null;
+	public static Placar placar = null;
+	public static int i = 0;
+	public static int rodada = 0;
+	public static boolean[] trocaDados = null;
+
+	public static void main(String[] args) throws InterruptedException {
+		trocaDados = new boolean[5];
+		rola  = new RolaDados(5);
+		placar = new Placar();
+		bozo = new GUIBozo();
+		dados = new GUIDados();
+		limpaTroca();
+		
+		JOptionPane.showMessageDialog(null, "Clique nos dados que deseja trocar,\nem seguida rolar para efetuar troca.\nClique no tabuleiro para selecionar a\nposicao que deseja fazer a jogada");
+	}
+	
+	public static void limpaTroca(){
+		for(int i = 0; i < 5; i++){
+			GUIMain.trocaDados[i] = false;
+			GUIDados.d.elementAt(i).setBackground(new Color(216,235,255));
+		}
+		
+	}
+
+}
