a862c5c7178f6da3e59777243b6f52b6a2a04b78
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-05-14 21:30:29 -0300

    Update GUIMain.java

--- Bozo/src/GUIMain.java
+++ Bozo/src/GUIMain.java
@@ -2,6 +2,10 @@ import java.awt.Color;
 
 import javax.swing.JOptionPane;
 
+/**
+ * @author Fabio Fogarin Destro 10284667
+ * @author Vitor Gratiere Torres 10284952
+ */
 public class GUIMain {
 	
 	private static GUIBozo bozo = null;
