dd0f9435ecba9c8823f278c3b139cb7f6f2690b1
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-05-17 18:07:20 -0300

    dice toggle

--- Bozo/src/DadoListener.java
+++ Bozo/src/DadoListener.java
@@ -10,8 +10,13 @@ public class DadoListener implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand());
-		GUIDados.d.elementAt(Integer.parseInt(e.getActionCommand())).setBackground(new Color(175,190,206));
-		GUIMain.trocaDados[Integer.parseInt(e.getActionCommand())] = true;
+		if(GUIMain.trocaDados[Integer.parseInt(e.getActionCommand())]){
+			GUIDados.d.elementAt(Integer.parseInt(e.getActionCommand())).setBackground(new Color(216,235,255));
+		}
+		else{
+			GUIDados.d.elementAt(Integer.parseInt(e.getActionCommand())).setBackground(new Color(175,190,206));
+		}
+		GUIMain.trocaDados[Integer.parseInt(e.getActionCommand())] = !(GUIMain.trocaDados[Integer.parseInt(e.getActionCommand())]);
 	}
 
 }
